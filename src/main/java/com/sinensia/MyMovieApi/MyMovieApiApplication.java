8a32a561a2b7cb27df0e1449726278bde77da5b7
Samyr Ahmad Sapsalyov <samyr.ahmad@gft.com>
2022-09-05 13:41:58 +0200

    Proyecto Inicializado

---
+++ src/main/java/com/sinensia/MyMovieApi/MyMovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.sinensia.MyMovieApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinensia/MyMovieApi/MyMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinensia.MyMovieApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
