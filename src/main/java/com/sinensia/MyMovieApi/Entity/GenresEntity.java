f575719fc6e761e7918dbdb0d0ffb0d2af93bda8
Samyr Ahmad Sapsalyov <samyr.ahmad@gft.com>
2022-09-06 09:09:16 +0200

    Packages with Entitis

---
+++ src/main/java/com/sinensia/MyMovieApi/Entity/GenresEntity.java
@@ -0,0 +1,57 @@
+package com.sinensia.MyMovieApi.Entity;
+
+
+
+public class GenresEntity {
+	
+	private int id;
+	private String name;
+	
+	
+	public GenresEntity() {
+		
+	}
+
+
+	public GenresEntity(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "GenresEntity {id=" + id + ", name=" + name + "}";
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+
+}
