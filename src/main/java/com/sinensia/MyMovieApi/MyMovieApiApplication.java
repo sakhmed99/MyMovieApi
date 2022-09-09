071acdcc4014deaa23ae29ffd67433fbfd457ae8
Samyr Ahmad Sapsalyov <samyr.ahmad@gft.com>
2022-09-09 11:14:26 +0200

    Adding Pivotal Gemfire

--- src/main/java/com/sinensia/MyMovieApi/MyMovieApiApplication.java
+++ src/main/java/com/sinensia/MyMovieApi/MyMovieApiApplication.java
@@ -3,11 +3,17 @@ package com.sinensia.MyMovieApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.gemfire.cache.config.EnableGemfireCaching;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 
 
 
 @SpringBootApplication
 @EnableCaching
+@ClientCacheApplication(name = "MyMovieApiApplication")
+@EnableGemfireCaching
+@EnableGemfireRepositories
 public class MyMovieApiApplication {
 
 	public static void main(String[] args) {
